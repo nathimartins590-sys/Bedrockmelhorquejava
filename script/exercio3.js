c22b64b033330ae66b1e9567cc119a7268980cdb
Nathalia <nathimartins590@gmail.com>
2026-02-26 21:09:07 -0300

    exercio

--- script/exercio3.js
+++ script/exercio3.js
@@ -2,14 +2,14 @@
 //a média desses pesos. Mostre o resultado
 
 //Entrada
-const peso1= Number(prompt("qual o peso da primeira pessoa?"))
-const peso2= Number(prompt("qual o peso da segunda pessoa?"))
-const peso3= Number(prompt("qual o peso da terceira pessoa?"))
-const peso4= Number(prompt("qual o peso da quarta pessoa?"))
-const peso5= Number(prompt("qual o peso da quinta pessoa?"))
+const peso1 = Number(prompt("qual o peso da primeira pessoa?"))
+const peso2 = Number(prompt("qual o peso da segunda pessoa?"))
+const peso3 = Number(prompt("qual o peso da terceira pessoa?"))
+const peso4 = Number(prompt("qual o peso da quarta pessoa?"))
+const peso5 = Number(prompt("qual o peso da quinta pessoa?"))
 
 //Processamento
-const ressultado = (peso1+peso2+peso3+peso4+peso5)
+const resultado = (peso1+peso2+peso3+peso4+peso5)/5
 
 //Saída
 alert(`A medida dos pesos informados é"${resultado}`)
