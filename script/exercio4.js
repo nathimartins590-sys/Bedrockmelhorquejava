dd6030e30ec0464602b836663eb5a1b81605dd81
Nathalia <nathimartins590@gmail.com>
2026-02-26 22:02:20 -0300

    exercio

---
+++ script/exercio4.js
@@ -0,0 +1,9 @@
+//Peça uma temperatura para
+//Converter Celcius para Fahrenheit
+
+const graus = Number(prompt("Coloque uma temperartura"))
+
+
+const resultado =((9*graus+ 160) /5)
+
+alert ("Fahrenheit" +resultado)
