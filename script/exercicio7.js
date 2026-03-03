abdebb8024b0dd4b269f3d9c80020b617ec786be
Nathalia <nathimartins590@gmail.com>
2026-03-02 22:12:05 -0300

    exercicio

---
+++ script/exercicio7.js
@@ -0,0 +1,8 @@
+//entrada
+const distancia = Number(prompt("Qual é a distancia em Km?"))
+//processo
+const resultado1 = distancia*1000
+const resultado2 = distancia*100000
+//saida
+alert(resultado1 + "m")
+alert(resultado2 + "cm")
---
+++ script/exercicio8.js
@@ -0,0 +1,24 @@
+//entrada
+const número = Number(prompt("Digite qualquer número"))
+//processamento
+const resultdo1 = número*1
+const resultdo2 = número*2
+const resultdo3 = número*3
+const resultdo4 = número*4
+const resultdo5 = número*5
+const resultdo6 = número*6
+const resultdo7 = número*7
+const resultdo8 = número*8
+const resultdo9 = número*9
+const resultdo10 = número*10
+//saida
+alert(resultdo1)
+alert(resultdo2)
+alert(resultdo3)
+alert(resultdo4)
+alert(resultdo5)
+alert(resultdo6)
+alert(resultdo7)
+alert(resultdo8)
+alert(resultdo9)
+alert(resultdo10)
