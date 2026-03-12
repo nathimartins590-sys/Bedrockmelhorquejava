738ed09c90ed75e6ea832c750320d85a5d6c4115
Nathalia <nathimartins590@gmail.com>
2026-03-12 20:01:04 -0300

    exercicio

---
+++ script/exemplo-if.js
@@ -0,0 +1,18 @@
+const numero = Number(prompt("Digite um numero"))
+
+// 9, 99, 100
+if (numero < 10){
+    alert("menor que 10")
+} 
+
+if (numero <100) {
+    alert("menor do que 100")
+}
+
+if (numero >10) {
+    alert("maior que 10")
+}
+
+if (numero >100) {
+    alert("maior que 100")
+}
