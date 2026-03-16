cc63a178012d9a6ff9bf2ad9651f5f0cf17f85b5
Nathalia <nathimartins590@gmail.com>
2026-03-16 19:42:23 -0300

    atividade

---
+++ old/exercicio10.js
@@ -0,0 +1,10 @@
+const sexo1 = H
+const sexo2 = M
+const sexo = Letra(prompt("digite seu genero"))
+const altura = Number(prompt("digite sua altura"))
+
+if(M = 62,1*altura) {
+    alert("seu peso ideal é:")
+} else if(H = 72,7*altura) {
+    alert("seu peso ideal é:")
+}
